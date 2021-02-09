6899f090f3230007414e2ea55196e8eb25d8d774
Shelly(Xiu) Chen <chenxiu0306@gmail.com>
2021-02-09 18:06:21 -0500

    Update please_work.py

--- dags/please_work.py
+++ dags/please_work.py
@@ -15,7 +15,7 @@ default_args = {
     "owner": "airflow",
     "depends_on_past": False,
     "start_date": datetime(2020, 2, 7),
-    "email": ["sharad.mishra@indexexchange.com"],
+    "email": ["shelly.chen@indexexchange.com"],
     "email_on_failure": True,
     "email_on_retry": True,
     "retries": 1,
